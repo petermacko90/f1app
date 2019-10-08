3aa5b9195c389f001488835c710d919f04ac6a46
Macko, Peter <peter.macko@accenture.com>
2019-10-08 15:52:30 +0200

    add themes for all teams

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ThemeService } from 'src/app/services/theme.service';
+import { TEAMS } from '../../constants/constants';
 
 @Component({
   selector: 'app-navigation',
@@ -7,6 +8,8 @@ import { ThemeService } from 'src/app/services/theme.service';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
+  teams = TEAMS;
+
   constructor(private themeService: ThemeService) { }
 
   saveTheme(theme: string) {
--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,2 +1,19 @@
 export const FIRST_SEASON = 1950;
 export const CURRENT_SEASON = new Date().getFullYear();
+
+interface Team {
+  id: string;
+  name: string;
+}
+export const TEAMS: Team[] = [
+  { id: 'mercedes', name: 'Mercedes' },
+  { id: 'ferrari', name: 'Ferrari' },
+  { id: 'redbull', name: 'Red Bull' },
+  { id: 'mclaren', name: 'McLaren' },
+  { id: 'renault', name: 'Renault' },
+  { id: 'tororosso', name: 'Toro Rosso' },
+  { id: 'racingpoint', name: 'Racing Point' },
+  { id: 'alfaromeo', name: 'Alfa Romeo' },
+  { id: 'haas', name: 'Haas' },
+  { id: 'williams', name: 'Williams' }
+];
