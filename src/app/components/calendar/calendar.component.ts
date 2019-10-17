bf6de58588408ed6d4f9547013ac26c37f3c9347
Macko, Peter <peter.macko@accenture.com>
2019-10-17 15:50:30 +0200

    move getting data from component to service

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { CalendarService } from '../../services/calendar.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Race, RaceDataSource } from '../../models/Race';
+import { RaceDataSource } from '../../models/Race';
 import { FIRST_SEASON, CURRENT_SEASON } from '../../constants/constants';
 import { MatSelectChange } from '@angular/material';
 
@@ -11,14 +13,11 @@ import { MatSelectChange } from '@angular/material';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-  calendars: { [k: string]: Race[] };
-  loading = false;
-  error = '';
   seasons: number[] = [];
   selectedSeason = CURRENT_SEASON;
   firstSeason = FIRST_SEASON;
   currentSeason = CURRENT_SEASON;
-  dataSource: RaceDataSource[];
+  dataSource$: Observable<RaceDataSource[]>;
   displayedColumns: string[] = ['round', 'location', 'date', 'time'];
 
   constructor(
@@ -28,88 +27,35 @@ export class CalendarComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    let season = Number(this.route.snapshot.paramMap.get('season'));
-
-    if (season === 0) {
-      season = this.currentSeason;
-      this.router.navigate([`/calendar/${season}`]);
-    }
-
-    if (!(season >= this.firstSeason && season <= this.currentSeason + 1)) {
-      this.error = 'Invalid season';
-    } else {
-      this.selectedSeason = season;
-      this.getCalendar(season);
-    }
-
     for (let i = this.firstSeason; i <= this.currentSeason + 1; i++) {
       this.seasons.push(i);
     }
-  }
 
-  getDate(date: string, time?: string) {
-    return time ? new Date(`${date}T${time}`) : new Date(date);
-  }
-
-  getUpcomingRace(races: Race[], season: number, currentSeason: number) {
-    if (season === currentSeason) {
-      for (let i = 0, l = races.length, d = new Date(); i < l; i++) {
-        if (d < this.getDate(races[i].date, races[i].time)) {
-          return races[i].round;
+    this.dataSource$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        let season = Number(params.get('season'));
+        if (
+          season === 0
+          || season < this.firstSeason
+          || season > this.currentSeason + 1
+          || Number.isNaN(season)
+        ) {
+          season = this.currentSeason;
+          this.selectedSeason = season;
+          this.router.navigate([`/calendar/${season}`]);
         }
-      }
-    }
-    return '';
-  }
-
-  getDataSource(calendar: Race[]): RaceDataSource[] {
-    const upcomingRace = this.getUpcomingRace(calendar, Number(calendar[0].season), this.currentSeason);
-
-    return calendar.map(race => ({
-      round: race.round,
-      location: `${race.Circuit.Location.country}, ${race.Circuit.Location.locality}`,
-      date: this.getDate(race.date, race.time).toLocaleDateString(),
-      time: race.time ? this.getDate(race.date, race.time).toLocaleTimeString() : 'N/A',
-      isUpcoming: upcomingRace === race.round
-    }));
-  }
-
-  getCalendar(season: number) {
-    if (this.calendars && this.calendars[season]) {
-      this.dataSource = this.getDataSource(this.calendars[season]);
-    } else {
-      this.error = '';
-      this.loading = true;
-      this.calendarService.getCalendar(season).subscribe(
-        data => {
-          if (data.MRData.RaceTable.Races.length === 0) {
-            this.error = 'No data available';
-          } else {
-            this.calendars = {
-              ...this.calendars,
-              ...{ [season]: data.MRData.RaceTable.Races }
-            };
-            this.dataSource = this.getDataSource(data.MRData.RaceTable.Races);
-          }
-          this.loading = false;
-        },
-        (error: string) => {
-          this.error = error;
-          this.loading = false;
-        }
-      );
-    }
+        return this.calendarService.getCalendar(season);
+      })
+    );
   }
 
   onSelectSeason(e: MatSelectChange) {
     this.selectedSeason = Number(e.value);
     this.router.navigate([`/calendar/${this.selectedSeason}`]);
-    this.getCalendar(this.selectedSeason);
   }
 
   onChangeSeason(change: number) {
     this.selectedSeason += change;
     this.router.navigate([`/calendar/${this.selectedSeason}`]);
-    this.getCalendar(this.selectedSeason);
   }
 }
--- src/app/services/calendar.service.ts
+++ src/app/services/calendar.service.ts
@@ -1,18 +1,65 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { RaceData, Race } from '../models/Race';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { RaceData, Race, RaceDataSource } from '../models/Race';
+import { CURRENT_SEASON } from '../constants/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalendarService {
-  baseUrl = 'https://ergast.com/api/f1';
-  calendars: { [k: string]: Race[] };
+  private baseUrl = 'https://ergast.com/api/f1';
+  private calendars: { [k: string]: Race[] };
+  private currentSeason = CURRENT_SEASON;
 
   constructor(private http: HttpClient) {}
 
-  getCalendar(season: number): Observable<RaceData> {
-    return this.http.get<RaceData>(`${this.baseUrl}/${season}.json`);
+  getCalendar(season: number): Observable<RaceDataSource[]> {
+    if (this.calendars && this.calendars[season]) {
+      return of(this.getDataSource(this.calendars[season]));
+    } else {
+      return this.http.get<RaceData>(`${this.baseUrl}/${season}.json`)
+        .pipe(
+          map(data => {
+            if (data.MRData.RaceTable.Races.length === 0) {
+              return [];
+            } else {
+              this.calendars = {
+                ...this.calendars,
+                ...{ [season]: data.MRData.RaceTable.Races }
+              };
+              return this.getDataSource(data.MRData.RaceTable.Races);
+            }
+          })
+        );
+    }
+  }
+
+  private getDate(date: string, time?: string) {
+    return time ? new Date(`${date}T${time}`) : new Date(date);
+  }
+
+  private getUpcomingRace(races: Race[], season: number, currentSeason: number) {
+    if (season === currentSeason) {
+      for (let i = 0, l = races.length, d = new Date(); i < l; i++) {
+        if (d < this.getDate(races[i].date, races[i].time)) {
+          return races[i].round;
+        }
+      }
+    }
+    return '';
+  }
+
+  private getDataSource(calendar: Race[]): RaceDataSource[] {
+    const upcomingRace = this.getUpcomingRace(calendar, Number(calendar[0].season), this.currentSeason);
+
+    return calendar.map(race => ({
+      round: race.round,
+      location: `${race.Circuit.Location.country}, ${race.Circuit.Location.locality}`,
+      date: this.getDate(race.date, race.time).toLocaleDateString(),
+      time: race.time ? this.getDate(race.date, race.time).toLocaleTimeString() : 'N/A',
+      isUpcoming: upcomingRace === race.round
+    }));
   }
 }
