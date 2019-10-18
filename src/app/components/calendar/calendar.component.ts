2b89871a7cd2891e3dd33938a553d84d7c9244f4
Macko, Peter <peter.macko@accenture.com>
2019-10-18 11:06:38 +0200

    change observable subscription

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
 import { CalendarService } from '../../services/calendar.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RaceDataSource } from '../../models/Race';
@@ -13,11 +11,12 @@ import { MatSelectChange } from '@angular/material';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
+  loading = false;
   seasons = SEASONS;
   selectedSeason = CURRENT_SEASON;
   firstSeason = FIRST_SEASON;
   currentSeason = CURRENT_SEASON;
-  dataSource$: Observable<RaceDataSource[]>;
+  dataSource: RaceDataSource[];
   displayedColumns: string[] = ['round', 'location', 'date', 'time'];
 
   constructor(
@@ -27,31 +26,40 @@ export class CalendarComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.dataSource$ = this.route.paramMap.pipe(
-      switchMap(params => {
-        let season = Number(params.get('season'));
-        if (
-          season === 0
-          || season < this.firstSeason
-          || season > this.currentSeason + 1
-          || Number.isNaN(season)
-        ) {
-          season = this.currentSeason;
-          this.selectedSeason = season;
-          this.router.navigate([`/calendar/${season}`]);
-        }
-        return this.calendarService.getCalendar(season);
-      })
+    let season = Number(this.route.snapshot.paramMap.get('season'));
+    if (
+      season === 0
+      || season < this.firstSeason
+      || season > this.currentSeason + 1
+      || Number.isNaN(season)
+    ) {
+      season = this.currentSeason;
+      this.selectedSeason = season;
+    } else {
+      this.selectedSeason = season;
+    }
+    this.getCalendar(season);
+  }
+
+  getCalendar(season: number) {
+    this.loading = true;
+    this.calendarService.getCalendar(season).subscribe(
+      (data) => {
+        this.dataSource = data;
+        this.loading = false;
+      }
     );
   }
 
   onSelectSeason(e: MatSelectChange) {
     this.selectedSeason = Number(e.value);
     this.router.navigate([`/calendar/${this.selectedSeason}`]);
+    this.getCalendar(this.selectedSeason);
   }
 
   onChangeSeason(change: number) {
     this.selectedSeason += change;
     this.router.navigate([`/calendar/${this.selectedSeason}`]);
+    this.getCalendar(this.selectedSeason);
   }
 }
